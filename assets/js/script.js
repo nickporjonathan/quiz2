565f6a2161a8e4f4051b3d30fadd89f36eace55e
nickporjonathan <nickporjonathan@gmail.com>
2022-01-27 18:36:47 -0500

    updated html to include all sections need for quiz application

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,17 @@ var countdown = function () {
 };
 
 var startCountdown = setInterval(countdown, 1000);
+
+// this funciton is for the button + when the user presses enter it will submit the form
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function () {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
